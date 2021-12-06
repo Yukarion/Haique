c95090d7eff67972acfb7585e4592e35724ae930
Mackyson <mackyson72@gmail.com>
2021-12-06 22:31:28 +0900

    timelineのページングの布石

--- APIserver/models/model_inline_object_5.go
+++ APIserver/models/model_inline_object_5.go
@@ -3,4 +3,10 @@ package models
 type InlineObject5 struct {
 
 	SessionId string `json:"session_id"`
+
+	// 最新のhaikuからN番目を起点とする（デフォルト0）
+	Start int32 `json:"start,omitempty"`
+
+	// 最新のhaikuからM番目を終点とする（デフォルト30）
+	End int32 `json:"end,omitempty"`
 }
