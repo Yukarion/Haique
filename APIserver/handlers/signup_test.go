b8900d6f1c9cfe2876ce416711c992469a100e0f
Mackyson <mackyson72@gmail.com>
2021-12-07 16:49:20 +0900

    落ちた時に読めるテストに変更

--- APIserver/handlers/signup_test.go
+++ APIserver/handlers/signup_test.go
@@ -20,32 +20,42 @@ var namepw = models.InlineObject{Name: "tatara", Pw: "fuga"}
 
 func TestSignup(t *testing.T) {
 	tests := []struct {
+		title    string
 		input    models.InlineObject
 		code     int
 		expected models.SessionId
 	}{
 		{
+			title:    "Create User",
 			input:    models.InlineObject{Name: "hoge", Pw: "fuga"},
 			code:     http.StatusCreated,
 			expected: models.SessionId{SessionId: "test"},
+		},
+		{
+			title:    "Username Conflict",
+			input:    models.InlineObject{Name: "hoge", Pw: "fuga"},
+			code:     http.StatusConflict,
+			expected: models.SessionId{SessionId: ""},
 		},
 	}
 
 	e := echo.New()
 	c, _ := NewContainerForTest(genUUIDForTest)
 	for _, test := range tests {
-		inputJson, _ := json.Marshal(test.input)
-		req := httptest.NewRequest(http.MethodPost, "/api/signup", strings.NewReader(string(inputJson)))
-		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-		rec := httptest.NewRecorder()
-		ctx := e.NewContext(req, rec)
+		t.Run(test.title, func(t *testing.T) {
+			inputJson, _ := json.Marshal(test.input)
+			req := httptest.NewRequest(http.MethodPost, "/api/signup", strings.NewReader(string(inputJson)))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+			ctx := e.NewContext(req, rec)
 
-		if assert.NoError(t, c.PostSignup(ctx)) {
-			var actual models.SessionId
-			json.Unmarshal(rec.Body.Bytes(), &actual)
-			assert.Equal(t, http.StatusCreated, rec.Code)
-			assert.Equal(t, test.expected.SessionId, actual.SessionId)
-		}
+			if assert.NoError(t, c.PostSignup(ctx)) {
+				var actual models.SessionId
+				json.Unmarshal(rec.Body.Bytes(), &actual)
+				assert.Equal(t, test.code, rec.Code)
+				assert.Equal(t, test.expected.SessionId, actual.SessionId)
+			}
+		})
 	}
 
 }
