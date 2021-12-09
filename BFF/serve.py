b645bd0bbee4f91aba5eab08489e8fd836292f62
Mackyson <mackyson72@gmail.com>
2021-12-09 20:06:08 +0900

    OpenAPIが生成するクライアントとFlaskの連携

--- BFF/serve.py
+++ BFF/serve.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
-import requests
+import openapi_client
+from openapi_client.api import default_api
 
 app = Flask(__name__)
+
+configuration = openapi_client.Configuration(
+    host = "http://api-server:8080"
+)
 
 @app.route("/")
 def hello_world():
@@ -9,12 +14,21 @@ def hello_world():
 
 @app.route("/top")
 def get_top():
-
     url = "http://api-server:8080/api/top"
 
-    r = requests.get(url)
-    print(r.json())
-    return render_template('top.html',title='top_30Haiku', Haikus=r.json())
+    with openapi_client.ApiClient(configuration=configuration) as api_client:
+        # Create an instance of the API class
+        api_instance = default_api.DefaultApi(api_client)
+
+        # example, this endpoint has no required or optional parameters
+        try:
+            # top
+            api_response = api_instance.get_top()
+        except openapi_client.ApiException as e:
+            print("Exception when calling DefaultApi->get_top: %s\n" % e)
+
+        #print(api_response.body.json())
+        return render_template('top.html',title='top_30Haiku', Haikus=api_response)
 
 ## おまじない
 if __name__ == "__main__":
