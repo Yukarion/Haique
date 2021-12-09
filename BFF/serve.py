4eb607b48276c7b6c2158fdb14c2c4ca7a8dcc1d
Mackyson <mackyson72@gmail.com>
2021-12-09 23:07:13 +0900

    signup/signin

--- BFF/serve.py
+++ BFF/serve.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
+
 import openapi_client
 from openapi_client.api import default_api
+from openapi_client.models import *
+
+from openapi_client.model.inline_object import InlineObject
+from openapi_client.model.inline_response201 import InlineResponse201
+
 
 app = Flask(__name__)
 
@@ -14,8 +20,6 @@ def hello_world():
 
 @app.route("/top")
 def get_top():
-    url = "http://api-server:8080/api/top"
-
     with openapi_client.ApiClient(configuration=configuration) as api_client:
         # Create an instance of the API class
         api_instance = default_api.DefaultApi(api_client)
@@ -26,9 +30,61 @@ def get_top():
             api_response = api_instance.get_top()
         except openapi_client.ApiException as e:
             print("Exception when calling DefaultApi->get_top: %s\n" % e)
+            resp = make_response(render_template("error.html",title="Error occured"))
+            return resp
 
-        #print(api_response.body.json())
         return render_template('top.html',title='top_30Haiku', Haikus=api_response)
+
+@app.route("/signup")
+def get_signup():
+    return render_template('signup.html',title='signup',err="")
+
+@app.route("/signup",methods=["POST"])
+def post_signup():
+    with openapi_client.ApiClient(configuration=configuration) as api_client:
+        # Create an instance of the API class
+        api_instance = default_api.DefaultApi(api_client)
+        inline_object = InlineObject(
+            name = request.form.get("name"),
+            pw = request.form.get("password"),
+        ) # InlineObject |  (optional)
+
+        # example passing only required values which don't have defaults set
+        # and optional values
+        try:
+            api_response = api_instance.post_signup(inline_object=inline_object)
+            print(api_response)
+            print(type(api_response))
+        except openapi_client.ApiException as e:
+            print("Exception when calling DefaultApi->post_signup: %s\n" % e)
+            return render_template('signup.html',title='signup',err="Already used name...")
+        resp = make_response(render_template('signup_done.html',title='signup'))
+        resp.set_cookie("session_id",api_response.session_id)
+        return resp
+
+@app.route("/signin")
+def get_signin():
+    return render_template('signin.html',title='signin',err="")
+@app.route("/signin",methods=["POST"])
+def post_signin():
+    with openapi_client.ApiClient(configuration=configuration) as api_client:
+        # Create an instance of the API class
+        api_instance = default_api.DefaultApi(api_client)
+        inline_object1 = InlineObject1(
+            name = request.form.get("name"),
+            pw = request.form.get("password"),
+        ) # InlineObject1 |  (optional)
+
+        # example passing only required values which don't have defaults set
+        # and optional values
+        try:
+            api_response = api_instance.post_signin(inline_object1=inline_object1)
+        except openapi_client.ApiException as e:
+            print("Exception when calling DefaultApi->post_signin: %s\n" % e)
+            return render_template('signin.html',title='signup',err="Wrong password or name")
+        resp = make_response(render_template('signup_done.html',title='signup'))
+        resp.set_cookie("session_id",api_response.session_id)
+        return resp 
 
 ## おまじない
 if __name__ == "__main__":
